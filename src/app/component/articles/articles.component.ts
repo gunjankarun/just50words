6a49bc4c9f2db5caf10ce92ae225a4749d0cd103
Gunjan Karun <gunjan@kalptaru.in>
2018-01-18 12:37:32 +0530

    Implemented automated bullets, indents and number while editing

--- src/app/component/articles/articles.component.ts
+++ src/app/component/articles/articles.component.ts
@@ -134,12 +134,22 @@ export class ArticlesComponent implements OnInit {
     this.update_summary();
     this.save_articles();
     this.celebrate = this._wordCountService.celebrate;
+
+    // Play the sound
     if (this._configService.play_keypress_sound) {
       const audio = new Audio(this._configService.keypress_sound);
       const abc = 'AAA';
       audio.play();
     }
 
+    switch (event.key) {
+      case 'Enter':
+        // format text
+        this.format_text();
+        break;
+      default:
+        break;
+    }
   }
 
   key_pressed_headline(event) {
@@ -302,7 +312,86 @@ export class ArticlesComponent implements OnInit {
     }else if (headline_length > 60 ) {
       this.headline_font = 'form-control font-small';
     }
-
   }
+
+  format_text() {
+
+    const start_pos = this.editor_object.selectionStart;
+    const end_pos = this.editor_object.selectionEnd;
+    // console.log ('selection start = ' + start_pos + ' and sel end = ' + end_pos);
+    const content_till_cursor = this.current_article.content.substr(0, start_pos);
+    // console.log('content_till_cursor', content_till_cursor);
+    const lines = content_till_cursor.split('\n');
+    const last_line = lines.length ? lines[lines.length - 2] : '';
+    const index_space = last_line.search(/\S|$/);
+    // console.log('index space', index_space);
+    let spaces = '';
+    if (index_space) {
+      for (let i = 0; i < index_space; i++) {
+        spaces = spaces + ' ';
+      }
+    }
+
+    // get bullet chars
+    const first_two_chars: string = last_line.substr(index_space, 2);
+    const arr_first_two_chars = first_two_chars.split('');
+    // console.log('First two chars are ', arr_first_two_chars);
+    if (arr_first_two_chars && arr_first_two_chars.length === 2) {
+      if (arr_first_two_chars[1] === ' ') {
+        const first_char = arr_first_two_chars[0];
+        const letters = /^[0-9a-zA-Z]+$/;
+        const is_alphanumeric = first_char.match(letters);
+        // console.log('Alpha numeric is ', is_alphanumeric);
+        if (!is_alphanumeric){
+          spaces = spaces + arr_first_two_chars.join('');
+        }
+      }
+    }
+
+    // Now create numbered list
+    const numbers_found = last_line.match(/(^[\s\d]+)(.+$)/i);
+    console.log('Numbers found', numbers_found);
+    if (numbers_found) {
+      const number_found = numbers_found[1];
+      const number_separator: string = last_line.substr(number_found.length, 2);
+      const arr_number_separator = number_separator.split('');
+      if (arr_number_separator && arr_number_separator.length === 2) {
+        if (arr_number_separator[1] === ' ') {
+          const first_char = arr_number_separator[0];
+          if (first_char === '.') {
+            const new_number = +number_found + 1;
+            spaces = spaces + new_number + arr_number_separator.join('');
+            // todo: now need to parse all future lines and if number found in future lines, then we update those numbers as well
+          }
+        }
+      }
+    }
+
+    // Update the text with bullets or indents
+    if (spaces) {
+      this.current_article.content = content_till_cursor + spaces + this.current_article.content.substr(end_pos, this.current_article.content.length);
+      // console.log('About to set selection start = ' + start_pos + ' and sel end = ' + end_pos);
+      this.editor_object.setSelectionRange(start_pos, end_pos);
+      // this.editor.nativeElement.setSelectionRange(start_pos, start_pos);
+      // const elem = document.getElementById('editor');
+      // elem.focus();
+      // elem.setSelectionRange(start_pos, start_pos);
+    }
+  }
+
+  // setSelectionRange(input, selectionStart, selectionEnd) {
+  //   if (input.setSelectionRange) {
+  //     console.log('in selection range');
+  //     input.focus();
+  //     input.setSelectionRange(selectionStart, selectionEnd);
+  //   } else if (input.createTextRange) {
+  //     console.log('in createTextRange');
+  //     const range = input.createTextRange();
+  //     range.collapse(true);
+  //     range.moveEnd('character', selectionEnd);
+  //     range.moveStart('character', selectionStart);
+  //     range.select();
+  //   }
+  // }
 
 }
