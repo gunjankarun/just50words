f344d8c61082d32f29172b2c7ac78d15ce1630f3
Gunjan Karun <gunjan@kalptaru.in>
2018-02-14 01:36:15 +0530

    Implemented the popup to show update info and show download link

--- src/app/component/update/update.component.ts
+++ src/app/component/update/update.component.ts
@@ -8,6 +8,7 @@ import { ConfigService } from '../../service/config.service';
 import { Constants } from '../../constants';
 import { UpdateService } from '../../service/update.service';
 import { ElectronService } from 'ngx-electron';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-update',
@@ -18,18 +19,21 @@ export class UpdateComponent implements OnInit {
   app_version = this._configService.app_version;
   git_username = Constants.git_username;
   git_repo_name = Constants.git_repo_name;
-  version_str = 'Current version: ' + this.app_version + ' (Checking for updates)';
+  version_str = 'Current version: ' +
+    this.app_version +
+    ' (Checking for updates)';
   version_url = Constants.product_website;
   update_data = {
     latest_version: '',
-    update_title: '',
-    update_msg: '',
+    title: '',
+    msg: '',
     release_url: ''
   };
   constructor(
     private _configService: ConfigService,
     private _updateService: UpdateService,
-    private _electronService: ElectronService
+    private _electronService: ElectronService,
+    private _modalService: NgbModal
   ) {}
 
   ngOnInit() {
@@ -37,44 +41,68 @@ export class UpdateComponent implements OnInit {
     this.check_update();
   }
 
-  check_update() {
-    this._updateService.check_release(this.git_username, this.git_repo_name)
-      .subscribe(http_data => {
+  show_update_popup(updatePopup) {
+    // will show options popup here
+    this._modalService.open(updatePopup).result.then(
+      result => {
+        // console.log(`Closed with: ${result}`);
+      },
+      reason => {
+        // console.log(`Dismissed ${reason}`);
+      }
+    );
+  }
 
+  check_update() {
+    this._updateService
+      .check_release(this.git_username, this.git_repo_name)
+      .subscribe(http_data => {
         if (http_data) {
           this.update_data.release_url = http_data.html_url;
           this.update_data.latest_version = http_data.tag_name;
-          this.update_data.update_title = '🎉   New version ' + http_data.tag_name + ' is available';
-          this.update_data.update_msg = http_data.body;
+          this.update_data.title =
+            '🎉   New version ' + http_data.tag_name + ' is available';
+          this.update_data.msg = http_data.body.replace(/\n/g, '<br>');
           // const new_version_str = http_data.tag_name.replace(/v/g, "");
           const arr_curr_version = this.app_version.split('.');
           const arr_new_version = http_data.tag_name.split('.');
           let new_version_available = false;
           if (arr_curr_version.length === 3 && arr_new_version.length === 3) {
-            if (parseInt(arr_new_version[0], 10) > parseInt(arr_curr_version[0], 10)) {
+            if (
+              parseInt(arr_new_version[0], 10) >
+              parseInt(arr_curr_version[0], 10)
+            ) {
               new_version_available = true;
-            } else if (parseInt(arr_new_version[1], 10) > parseInt(arr_curr_version[1], 10)) {
+            } else if (
+              parseInt(arr_new_version[1], 10) >
+              parseInt(arr_curr_version[1], 10)
+            ) {
               new_version_available = true;
-            } else if (parseInt(arr_new_version[2], 10) > parseInt(arr_curr_version[2], 10)) {
+            } else if (
+              parseInt(arr_new_version[2], 10) >
+              parseInt(arr_curr_version[2], 10)
+            ) {
               new_version_available = true;
             } else {
-              this.update_data.update_title = 'You have the latest version';
+              this.update_data.title = 'You have the latest version';
             }
 
-            if ( new_version_available ) {
-              this.version_str = this.update_data.update_title;
+            if (new_version_available) {
+              this.version_str = this.update_data.title;
               this.version_url = this.update_data.release_url;
 
               // Now let us get the version url based on the os.
               const asset_file_ext = this.get_file_extension();
               if (asset_file_ext) {
-                console.log('asset_file_ext=', asset_file_ext );
+                console.log('asset_file_ext=', asset_file_ext);
                 http_data.assets.forEach(asset => {
                   const dl_url = asset.browser_download_url;
                   console.log('Checking url ' + dl_url);
                   const arr_dl_url = dl_url.split('.');
                   const dl_ext = arr_dl_url[arr_dl_url.length - 1];
-                  console.log('Comparing ext = ' + dl_ext + ' with ' +  asset_file_ext);
+                  console.log(
+                    'Comparing ext = ' + dl_ext + ' with ' + asset_file_ext
+                  );
                   if (dl_ext.toLowerCase() === asset_file_ext) {
                     this.version_url = dl_url;
                     return;
@@ -82,8 +110,8 @@ export class UpdateComponent implements OnInit {
                 });
               }
             }
-          }else {
-            this.update_data.update_title = 'Could not check updates';
+          } else {
+            this.update_data.title = 'Could not check updates';
           }
         }
         // console.log('update_data', this.update_data);
@@ -120,5 +148,4 @@ export class UpdateComponent implements OnInit {
     // console.log('OS = ' + os + ' and file_ext = ' + file_ext);
     return file_ext;
   }
-
 }
